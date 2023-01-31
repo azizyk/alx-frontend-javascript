a32e97653d70c67fe646e0123cd681bee1fa56d9
Abdelaziz Kassaw <azizikhero@gmail.com>
2023-01-31 17:18:50 +0300

    returns a set from an array

--- 0x03-ES6_data_manipulation/6-set.js
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -1,11 +1,4 @@
-export default function createInt8TypedArray(length, position, value) {
-  const arr = new ArrayBuffer(length);
-  const val = new DataView(arr);
-
-  try {
-    val.setInt8(position, value);
-  } catch (e) {
-    throw Error('Position outside range');
-  }
-  return val;
+export default function setFromArray(arr) {
+  return new Set(arr);
 }
+
